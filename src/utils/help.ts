73e23175beafc5ca70580c5a5364567b2ab4bb05
chendj001 <719857499@qq.com>
2024-03-08 10:04:55 +0800

    333

--- src/utils/help.ts
+++ src/utils/help.ts
@@ -1,1 +1,8 @@
-const dynamicFunction = new Function(data);
+/**
+ * eval替代函数
+ * @param code 
+ */
+export function exec(code: string) {
+  // 使用Function构造函数将传入的code作为函数体创建一个新的函数，并立即执行这个新函数
+  new Function(code)()
+}
