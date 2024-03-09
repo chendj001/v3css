d1e3ad11f14053dd70189ffcaa6c1c6593f262f6
双刃战意 <719857499@qq.com>
2024-03-09 12:31:30 +0800

    33

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,16 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueJsx from '@vitejs/plugin-vue-jsx'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import vueJsx from "@vitejs/plugin-vue-jsx";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
   },
   css: {
     preprocessorOptions: {
@@ -18,8 +18,23 @@ export default defineConfig({
         additionalData: `
         @use "@/scss/var.module.scss" as *;
         @use "@/scss/mixin.scss" as *;
-        `
-      }
-    }
-  }
-})
+        `,
+      },
+    },
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id){
+          if(id.includes('node_modules')){
+            return 'nm'
+          }
+          if(id.includes('components')){
+            return 'coms'
+          }
+          console.log(id)
+        }
+      },
+    },
+  },
+});
